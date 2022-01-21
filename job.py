e17ed030ec3b6d0e823a67ef838cc427b2798e95
Clementol <clementolaniyan95@gmail.com>
2022-01-21 01:41:00 +0100

    updates

--- job.py
+++ job.py
@@ -4,41 +4,47 @@ import csv
 def get_csv_data():
     with open("jobs_dataset_dup.csv") as file:
         reader = csv.reader(file)
-        count = 0
         data = []
+        count = 0
         for row in reader:
             count += 1
             data.append(row)
 
-            # if count > 8:
+            # if count > 814:
             #     break
     # numbers_to_search = data[1:][0][1:6]
     data_to_search_from = data[1:]
     # for i in range(len(data_to_search_from)):
-    numbers_to_search = data_to_search_from[0][1:6]
+    numbers_to_search = data_to_search_from[1][1:6]
     counts = 0
-    search_data(numbers_to_search, data_to_search_from, counts)
+    position = 0
+    search_data(numbers_to_search, data_to_search_from, counts, position)
 
 
-def search_data(numbers_to_search, data_to_search_from, counts):
+def search_data(numbers_to_search, data_to_search_from, counts, position):
     # print(numbers_to_search)
-    # counts = 0
-    # data_to_search_from = data_to_search_from
-    md = data_to_search_from
+    modified_data = []
+
+    data_to_search_from = data_to_search_from
+    # md = data_to_search_from
     for index, item in enumerate(data_to_search_from):
         if numbers_to_search == item[1:6]:
             counts += 1
-            md[index][-1] = 0
-            # print(index, md[index])
+            item[-1] = position
+            modified_data.append(item)
             data_to_search_from.pop(index)
             # del data_to_search_from[index]
 
     print(counts, numbers_to_search, len(data_to_search_from))
 
+    if position == 0:
+        return
     if len(data_to_search_from) > 0:
+        position += 1
         next_row_to_search = data_to_search_from[0][1:6]
         counts = 0
-        search_data(next_row_to_search, data_to_search_from, counts)
+        search_data(next_row_to_search, data_to_search_from, counts, position)
+    # print(modified_data)
 
 
 get_csv_data()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-
+import csv
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -7,9 +7,20 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-li = [1, 2, 3, 4]
-for i in range(len(li)):
-    print(li[i])
+
+
+with open("jobs_dataset_dup.csv") as file:
+    reader = csv.reader(file)
+    data = []
+    for row in reader:
+        data.append(row)
+
+        # if count > 8:
+        #     break
+    # numbers_to_search = data[1:][0][1:6]
+data_to_search_from = data[1:]
+for i, item in enumerate(data_to_search_from):
+    print(item)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
